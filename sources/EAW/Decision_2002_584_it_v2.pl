fb017d898dc700c798bef3521887c2c515164dc4
Alessandro Parenti <alecrypto6@protonmail.com>
2024-05-07 18:07:29 +0200

    adattato decision_it_v2 fino a art. 19 escluso

--- sources/EAW/Decision_2002_584_it_v2.pl
+++ sources/EAW/Decision_2002_584_it_v2.pl
@@ -44,7 +44,7 @@ mandatory_refusal(article18_1_b, MemberState, europeanArrestWarrant):-
 
 mandatory_refusal(article18_1_b, MemberState, europeanArrestWarrant):-
     eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence),
-    person_event(PersonId, irrevocably_convicted, Offence),
+    person_event(PersonId, finally_judged, Offence),
     (
         sentence_served(PersonId)
     ;   sentence_being_served(PersonId)
@@ -78,12 +78,12 @@ person_status(PersonId, under_age, ExecutingMemberState):-
 
 optional_refusal(article18bis_1_a, MemberState, europeanArrestWarrant):-
     eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence),
-    proceeding_status(Offence, MemberState, committed_inside_territory).
+    executing_proceeding_status(Offence, ExecutingMemberState, committed_inside_territory).
 
 optional_refusal(article18bis_1_a, MemberState, europeanArrestWarrant):-
     eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence),
-    proceeding_status(Offence, IssuingMemberState, committed_outside_territory),
-    proceeding_status(Offence, MemberState, no_prosecution).
+    issuing_proceeding_status(Offence, IssuingMemberState, committed_outside_territory),
+    prosecution_not_allowed(Offence, ExecutingMemberState).
 
 %Article 18bis(1)(b) - Fully implemented
 % 1. When the European arrest warrant has been issued for the purpose of prosecution in criminal matters, the Court of Appeal may refuse surrender in the following cases
@@ -91,20 +91,24 @@ optional_refusal(article18bis_1_a, MemberState, europeanArrestWarrant):-
 
 optional_refusal(article18bis_1_b, MemberState, europeanArrestWarrant):-
     eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence),
-    person_event(PersonId, under_prosecution, Offence).
+    person_event(PersonId, under_prosecution_by_executing_state, Offence).
 
 % Article 18(2-2bis) - Fully implemented
 % 2. When the European arrest warrant has been issued for the purpose of the execution of a custodial sentence or a security measure, the Court of Appeal may refuse to surrender of the Italian citizen or of a person who lawfully and effectively resides continuously for at least five years on the Italian territory, as long as it also orders that such sentence or security measure be executed in Italy in accordance with its domestic law.
 
-optional_refusal(article18_2, MemberState, europeanArrestWarrant):-
+optional_refusal(article18bis_2, MemberState, europeanArrestWarrant):-
     eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence),
     (
         executing_proceeding(ExecutingMemberState, PersonId, execution_custodial_sentence)
     ;   executing_proceeding(ExecutingMemberState, PersonId, execution_detention_order)
     ),
-    person_residence(PersonId, lawful_effective_residence, MemberState, Time),
-    Time >= 5.
-
+    (
+        person_citizenship(PersonId, italian)
+    ;   (person_continuous_residence(PersonId, ExecutingMemberState, Time),
+        Time >= 5)
+    ).
+    
+    
 % 2-bis. For the purposes of verifying the lawful and effective residence on the Italian territory of the person requested to be surrendered, the Court of Appeal shall ascertain whether the execution of the sentence or of the security measure on the territory is in fact suitable to enhance the person's chances of social reintegration, taking into account: the duration, nature and modalities of residence; the time elapsed between the commission of the offence on the basis of which the European arrest warrant was issued and the beginning of the period of residence; the commission of offences and the regular fulfilment of social security and tax obligation duties during that period; the compliance with national rules on the entry and residence of foreigners, as well the family ties, linguistic, cultural, social, economic or other ties that the person possesses on Italian territory and any other relevant element. The judgment is null and void if it does not contain the specific indication of the elements referred above and the relevant evaluation criteria
 
 person_residence(PersonId, lawful_effective_residence, MemberState, Time):-
@@ -121,37 +125,35 @@ optional_refusal(article61bis, MemberState, europeanArrestWarrant):-
     ;   proceeding_status(Offence, MemberState, execution_detention_order)
     ;   proceeding_status(Offence, MemberState, deprivation_of_liberty)
     ),
-    proceeding_status(Offence, IssuingMemberState, trial_in_absentia),
-    issuing_member_state(PersonId, IssuingMemberState),
-    \+ exception(optional_refusal(article61bis, _, europeanArrestWarrant), _).
+    issuing_proceeding_status(Offence, IssuingMemberState, trial_in_absentia),
+    \+ exception(optional_refusal(article61bis, ExecutingMemberState, europeanArrestWarrant), _).
 
 %(a) the person concerned has been summoned in a timely manner in person or in such a way as to guarantee unequivocally that he or she was aware of the time and place of the trial which led to the decision rendered in his or her absence and of the fact that such decision could also have been made in his absence.
 
-exception(optional_refusal(article61bis, _, europeanArrestWarrant), article61bis_a):-
-    person_event(PersonId, aware_trial, Offence),
-    person_event(PersonId, informed_decision, Offence).
+exception(optional_refusal(article61bis, ExecutingMemberState, europeanArrestWarrant), article61bis_a):-
+    issuing_proceeding_event(PersonId, Offence, aware_trial),
+    issuing_proceeding_event(PersonId, Offence, informed_of_potential_decision).
 
 %(b) the person concerned, having been informed of the proceedings against him, was represented at the trial resulting in the abovementioned decision by a legal counsellor, either appointed by the person concerned or ex officio.
 
-exception(optional_refusal(article61bis, _, europeanArrestWarrant), article61bis_b):-
-    person_event(PersonId, aware_trial, Offence),
-    person_event(PersonId, presence_legal_defence, Offence).
+exception(optional_refusal(article61bis, ExecutingMemberState, europeanArrestWarrant), article61bis_b):-
+    issuing_proceeding_event(PersonId, Offence, mandated_legal_defence).
 
 %(c) the person concerned, having been served with the decision of which enforcement is sought and informed of the right to a retrial or of the right to appeal, in which he or she has the right to participate and which allows a review of the merits of the decision, as well as by means of the bringing of new evidence, the possibility of its reform, has declared expressly stated that he or she does not contest that decision or did not request the renewal of the trial or lodged an appeal within the prescribed time limit.
 
-exception(optional_refusal(article61bis, _, europeanArrestWarrant), article61bis_c):-
-    person_event(PersonId, informed_of_right_retrial_appeal, Offence),
+exception(optional_refusal(article61bis, ExecutingMemberState, europeanArrestWarrant), article61bis_c):-
+    issuing_proceeding_event(PersonId, Offence, informed_of_right_retrial_appeal),
     (
-        person_event(PersonId, does_not_contest_decision, Offence)
-    ;   person_event(PersonId, does_not_request_retrial_appeal, Offence)
+        issuing_proceeding_event(PersonId, Offence, does_not_contest_decision)
+    ;   issuing_proceeding_event(PersonId, Offence, does_not_request_retrial_appeal)
     ).
 
 %(d) the person concerned has not been personally served with the decision, but will receive it personally and without delay after the surrender in the issuing Member State and will be expressly informed either of the right to a retrial or to lodge an appeal, in which he or she has the right to participate and which allows a review on the merits, as well as, including by through the bringing of new evidence, the possibility of a reform of that decision, and of the time-limits within which he may request a new trial or lodge an appeal for the appeal.
 
 exception(optional_refusal(article61bis, _, europeanArrestWarrant), article61bis_d):-
-    person_event(PersonId, not_personally_served_decision, Offence),
-    person_event(PersonId, informed_of_right_retrial_appeal, Offence),
-    person_event(PersonId, informed_of_timeframe_retrial_appeal, Offence).
+    issuing_proceeding_event(PersonId, Offence, not_personally_served_decision),
+    issuing_proceeding_event(PersonId, Offence, informed_of_right_retrial_appeal),
+    issuing_proceeding_event(PersonId, Offence, informed_of_timeframe_retrial_appeal).
 
 %% Article 18-ter - Fully implemented
 % 1. When the European arrest warrant has been issued for the purpose of the execution of a custodial sentence or a detention order imposed as a result of a trial at which the person concerned did not appear in person, the court of appeal may also refuse surrender if the European arrest warrant does not contain the indication of any of the conditions set out in Article 6(1a) and the issuing State has not indicated any such conditions even following a request made pursuant to Article 16.
