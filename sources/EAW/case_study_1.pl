2f5b8e0033963f0545cb6af6c6e1232367ac4677
Billuz <marcobilli97@gmail.com>
2024-05-13 10:37:39 +0200

    add files with only case studies rules

---
+++ sources/EAW/case_study_1.pl
@@ -0,0 +1,80 @@
+:- include('../utils.pl').
+
+%%Decision 2002/584
+%%Council Framework Decision of 13 June 2002 on the European arrest warrant and the surrender procedures between Member States (2002/584/JHA)
+
+%%Articles 1-2
+%1. The European arrest warrant is a judicial decision issued by a Member State with a view to the arrest and surrender by another Member State of a requested person, for the purposes of conducting a criminal prosecution or executing a custodial sentence or detention order.
+
+eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence):-
+    issuing_proceeding(IssuingMemberState, PersonId, Offence),
+    (
+        art2_2applies(Offence)
+    ;   art2_4applies(Offence)
+    ),
+    (
+        executing_proceeding(ExecutingMemberState, PersonId, criminal_prosecution)
+    ;   executing_proceeding(ExecutingMemberState, PersonId, execution_custodial_sentence)
+    ;   executing_proceeding(ExecutingMemberState, PersonId, execution_detention_order)
+    ).
+
+art2_4applies(Offence):-
+    \+ art2_2applies(Offence).
+
+%3. if the person who is the subject of the European arrest warrant may not, owing to his age, be held criminally responsible for the acts on which the arrest warrant is based under the law of the executing State.
+
+%[executing_proceeding(ExecutingMemberState, PersonId, _)] = if the person who is the subject of the European arrest warrant
+%[person_status(PersonId, under_age, ExecutingMemberState)] = may not, owing to his age, be held criminally responsible [..] under the law of the executing State
+
+mandatory_refusal(article3_3, ExecutingMemberState, europeanArrestWarrant):-
+    eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence),
+    person_status(PersonId, under_age, ExecutingMemberState).
+
+%%Article 4
+%Grounds for optional non-execution of the European arrest warrant
+
+%The executing judicial authority may refuse to execute the European arrest warrant:
+    
+%1. if, in one of the cases referred to in Article 2(4), the act on which the European arrest warrant is based does not constitute an offence under the law of the executing Member State; however, in relation to taxes or duties, customs and exchange, execution of the European arrest warrant shall not be refused on the ground that the law of the executing Member State does not impose the same kind of tax or duty or does not contain the same type of rules as regards taxes, duties and customs and exchange regulations as the law of the issuing Member State;
+
+optional_refusal(article4_1, ExecutingMemberState, europeanArrestWarrant):-
+    art2_4applies(Offence),
+    eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence),
+    national_law_not_offence(Offence, ExecutingMemberState).
+
+%% Legge 22 aprile 2005, n. 69
+%% Provisions to adapt national legislation to the Council Framework Decision 2002/584/JHA of 13 June 2002 on the European arrest warrant and the surrender procedures between Member States
+
+%% Art. 1
+%1.[...]
+%2. Il mandato d'arresto europeo è una decisione giudiziaria emessa da uno Stato membro dell'Unione europea, di seguito denominato "Stato membro di emissione", in vista dell'arresto e della consegna da parte di un altro Stato membro, di seguito denominato "Stato membro di esecuzione", di una persona, al fine dell'esercizio di azioni giudiziarie in materia penale o dell'esecuzione di una pena o di una misura di sicurezza privative della libertà personale.
+
+eaw_matter(PersonId, IssuingMemberState, ExecutingMemberState, Offence):-
+    issuing_proceeding(IssuingMemberState, PersonId, Offence),
+    crime_type(Offence, committed_in(_)),
+    (
+        executing_proceeding(ExecutingMemberState, PersonId, criminal_prosecution)
+    ;   executing_proceeding(ExecutingMemberState, PersonId, execution_custodial_sentence)
+    ;   executing_proceeding(ExecutingMemberState, PersonId, execution_detention_order)
+    ).
+
+% c) if the person who is the subject of the European arrest warrant was a minor 14 years of age at the time of the commission of the offence.
+
+mandatory_refusal(article18_1_c, italy, europeanArrestWarrant):-
+    eaw_matter(PersonId, IssuingMemberState, italy, Offence),
+    person_status(PersonId, under_age, italy).
+
+person_status(PersonId, under_age, italy):-
+    person_age(PersonId, Age),
+    Age =< 14.
+
+%% Article 7(1-2) - Fully implemented
+% 1. Italy shall execute the European arrest warrant only where the act constitutes a criminal offence under national law, irrespective of its legal classification and the single constituent elements of the offence.
+% 2. For the purposes of paragraph 1, for offences relating to taxes, customs and exchanges, it is not necessary that Italian law imposes the same kind of taxes or duties or contains the same kind of tax, duty, customs and exchange regulations as the law of the issuing Member State.
+
+mandatory_refusal(article7_1, MemberState, europeanArrestWarrant):-
+    eaw_matter(PersonId, IssuingMemberState, italy, Offence),
+    national_law_not_offence(Offence, MemberState).
+
+national_law_not_offence(Offence, ExecutingMemberState):-
+    cassazione(numero_41102_2022, reato_di_guida, italia).
---
+++ sources/EIO/case_study_2.pl
@@ -0,0 +1,44 @@
+:- include('../utils.pl').
+
+%%Article 1
+
+%The European Investigation Order and obligation to execute it
+
+%1.   A European Investigation Order (EIO) is a judicial decision which has been issued or validated by a judicial authority of a Member State (‘OGthe issuing State’) to have one or several specific investigative measure(s) carried out in another Member State (‘the executing State’) to obtain evidence in accordance with this Directive.
+%The EIO may also be issued for obtaining evidence that is already in the possession of the competent authorities of the executing State.
+
+directive_matter(IssuingMemberState, ExecutingMemberState, Measure):-
+    issuing_proceeding(IssuingMemberState, _, Offence),
+    executing_member_state(ExecutingMemberState, PersonId, Measure),
+    (
+        art4_a_applies
+    ;   art4_b_applies
+    ;   art4_c_applies
+    ;   art4_d_applies
+    ).
+
+%%(c) the EIO has been issued in proceedings referred to in Article 4(b) and (c) and the investigative measure would not be authorised under the law of the executing State in a similar domestic case;
+
+optional_refusal(article11_1_c, ExecutingMemberState, europeanInvestigationOrder):-
+    directive_matter(IssuingMemberState, ExecutingMemberState, Measure),
+    (
+        art4_b_applies
+    ;   art4_c_applies
+    ),
+    national_law_does_not_authorize(ExecutingMemberState, Measure).
+
+%%Article 694-31(4) - Partially implemented
+%The magistrate to whom the case is referred refuses to recognize or execute a European investigation order in any of the following cases:
+
+%4. If the request concerns proceedings referred to in Article 694-29 of this Code that do not relate to a criminal offence, where the measure requested would not be authorised under French law in the context of similar national proceedings;
+
+optional_refusal(article694_31_4, ExecutingMemberState, europeanInvestigationOrder):-
+    eio_matter(IssuingMemberState, ExecutingMemberState, Measure),
+    (
+        art694_29_applies
+    ),
+    national_law_does_not_authorize(ExecutingMemberState, Measure).
+
+national_law_does_not_authorize(ExecutingMemberState, Measure):-
+    %Article 694-28 does not provide a specific ground for refusing the investigative measure where it is not authorised in a similar domestic procedure. However, the provision can be considered as fully transposed, since article 694-38 of the Code of Criminal Procedure provides for the general possibility for the investigating magistrate to refuse to carry out the requested investigative measure where it could not be carried out in similar domestic cases and there is no other investigative measure that would make it possible to obtain the information requested by the issuing authority.
+    authority_decision(Measure, investigating_magistrate, ExecutingMemberState).
