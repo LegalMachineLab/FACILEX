0b5a7e731fb233e6e1aa74df8157ec1daec37a6b
Billuz <marcobilli97@gmail.com>
2024-09-30 12:38:51 +0200

    fixes

--- sources/EAW/Decision_2002_584_v3.pl
+++ sources/EAW/Decision_2002_584_v3.pl
@@ -239,19 +239,3 @@ optional_refusal(article5_3, ExecutingMemberState, europeanArrestWarrant):-
     ;   person_residence(PersonId, ExecutingMemberState)
     ),
     \+ guarantee(PersonId, Offence, return_to_executing_state).
-
-
-
-
-    
-offence_type(theft).
-art2_2applies(theft).
-issuing_member_state(italy).
-executing_member_state(france).
-executing_proceeding_purpose(marco, execution_detention_order).
-person_role(marco, subject_eaw).
-offence_committed_in(italy).
-amnesty(theft, france).
-
-person_event(marco, finally_judged, theft).
-sentence_served(marco).
---
+++ sources/EAW/facts_test.pl
@@ -0,0 +1,11 @@
+offence_type(theft).
+art2_2applies(theft).
+issuing_member_state(italy).
+executing_member_state(france).
+executing_proceeding_purpose(marco, execution_detention_order).
+person_role(marco, subject_eaw).
+offence_committed_in(italy).
+amnesty(theft, france).
+
+person_event(marco, finally_judged, theft).
+sentence_served(marco).
